f1a96559cdaf7b2d3fda47f7edcf4f81fd1f3dab
Haikal Hilmi <haikal6255hilmi@gmail.com>
2022-11-13 21:02:56 +0700

    feat : submit

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [name, setName] = useState('Haikal');
+  const HandleClick = () => {
+    setName('Hilmi');
+  };
   return (
-    <div className='text-bold uppercase text-red-600'>halo</div>
+    <div>
+      <div className='text-bold uppercase text-red-600 justify-center justi'>{name}</div>
+      <button type="submit" onClick={HandleClick} >submit</button>
+    </div>
+    
+    
   )
 }
