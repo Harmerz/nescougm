c861e2d2a432c707f55db8d826cee90034dc9f30
Afix Vega Praditya <afixv00@gmail.com>
2022-11-15 22:56:05 +0700

    feat: add button component

---
+++ components/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Button = ({ children, color, size }) => {
+  const buttonColor = {
+    primary: "bg-gradient-to-r from-[#9806F2] to-[#689FE3] opacity-90 hover:opacity-100 border-[0.5px]",
+    secondary: "bg-gradient-to-r from-[#FFA62E] to-[#AF7CCE]",
+  };
+
+  const buttonSize = {
+    sm: "text-sm",
+    md: "text-base",
+    lg: "text-lg",
+  };
+
+  const colorClassName = buttonColor[color || "primary"];
+  const sizeClassName = buttonSize[size || "md"];
+
+  return <button className={`px-12 py-4 rounded-full ${colorClassName} ${sizeClassName} font-bold text-white opacity-90 hover:opacity-100 transition-opacity`}>{children}</button>;
+};
+
+export default Button;
