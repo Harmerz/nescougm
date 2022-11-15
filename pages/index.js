5d9f776d86b3eab6c057c61bea633173a3ea0285
Afix Vega Praditya <afixv00@gmail.com>
2022-11-15 23:55:06 +0700

    feat: add button and back to top

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css';
 import { useState } from 'react';
+import Button from '../components/Button'
+import BackToTop from '../components/BackToTop';
 
 export default function Home() {
   const [name, setName] = useState('Haikal');
@@ -9,9 +11,12 @@ export default function Home() {
     setName('Hilmi');
   };
   return (
-    <div>
+    <div className='h-[200vh]'>
       <div className='text-bold uppercase text-red-600 justify-center justi'>{name}</div>
       <button type="submit" onClick={HandleClick} >submit</button>
+      <Button color='primary'> Button </Button>
+      <Button color='secondary'> Button </Button>
+      <BackToTop />
     </div>
     
     
