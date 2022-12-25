b90bcfd44cc93c002f84cb4c2bff5a7b02769c6a
Haikal Hilmi <haikal6255hilmi@gmail.com>
2022-12-25 23:01:34 +0700

    feat : pages

--- pages/competition.js
+++ pages/competition.js
@@ -6,9 +6,9 @@ import { Button } from '../components/element/Button';
 import { BackToTop } from '../components/element/BackToTop';
 import { DefaultLayout } from '../components/layout';
 
-export default function Home() {
+export default function Competition() {
   return (
-    <DefaultLayout title="Homepage - NESCO UGM 2023">
+    <DefaultLayout title="Competition - NESCO UGM 2023">
     <div className='h-[200vh]'>
       <div className='text-bold uppercase text-red-600 justify-center justi'>Haikal</div>
       <Button color='primary'> Button </Button>
