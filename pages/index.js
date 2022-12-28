368d1a1c3078f556707dfc487bb5a2cb53ff2fda
Haikal Hilmi <haikal6255hilmi@gmail.com>
2022-12-28 17:03:24 +0700

    feat : hero to component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import { BackToTop } from '../components/element/BackToTop';
 import { DefaultLayout } from '../components/layout';
 import { Footer } from '../components/layout';
+import { Hero, Photo, About } from '../components/pages/homepage';
 
 
 export default function Home() {
   return (
     <DefaultLayout title="Homepage - NESCO UGM 2023">
-    <div className='justify-center bg-gradient-to-br from-bg-00 to-bg-01 h-[100vh] flex items-center'>
-      <div className='text-7xl font-bold uppercase text-white'>Homepage</div>
+    <div>
+      <Hero />
+      <About />
+      <Photo />
       <BackToTop />
     </div>
       
