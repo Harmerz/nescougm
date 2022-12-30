1237bede5df3c4d0e7b813a85e4faf4e387a7367
Afix Vega Praditya <afixv00@gmail.com>
2022-12-30 16:48:53 +0700

    fix: style button

--- components/element/button/Button.js
+++ components/element/button/Button.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Button = ({ children, type, size }) => {
   const buttonType = {
-    primary: "bg-c-01 text-c-00",
-    secondary: "bg-c-00 text-c-01 border-4 border-c-01",
+    primary: "bg-c-01 text-c-00 hover:opacity-80 ",
+    secondary: "bg-c-00 text-c-01 border-4 border-c-01 hover:bg-c-01 hover:text-c-00",
   };
 
   const buttonSize = {
@@ -15,5 +15,5 @@ export const Button = ({ children, type, size }) => {
   const colorClassName = buttonType[type || "primary"];
   const sizeClassName = buttonSize[size || "md"];
 
-  return <button className={`px-12 py-4 rounded-full ${colorClassName} ${sizeClassName} font-bold opacity-90 hover:opacity-100 transition-opacity`}>{children}</button>;
+  return <button className={`px-12 py-4 rounded-full ${colorClassName} ${sizeClassName} transition-all font-bold`}>{children}</button>;
 };
