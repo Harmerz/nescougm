86eb311af4fbdf29da6c2a0981c48e55e9a4b14d
unknown <zildirayalfirli@gmail.com>
2023-01-12 19:33:55 +0700

    add homepage webinar

---
+++ components/pages/homepage/Webinar.js
@@ -0,0 +1,23 @@
+export function Webinar() {
+  return (
+    <div className="bg-bg-02 h-[100vh]">
+      <div className="grid h-[100vh]">
+        <div className="grid h-[20vh] justify-center row-start-1">
+          <div className="self-end place-self-center text-[22px] sm:text-[42px] text-justify font-bold font-poppins leading:[20px] lg:leading-[43.15px] text-transparent bg-clip-text bg-gradient-to-br from-c-01 to-c-02">
+            {' '}
+            Lorem Ipsum sit dolor amet{' '}
+          </div>
+        </div>
+
+        <div className="lg:flex grid justify-center h-[80vh]">
+          <div className="h-[30vh] justify-self-center self-center box-border h-[157px] w-[285px] sm:h-[220px] sm:w-[350px] xl:h-[271px] xl:w-[438px] shadow-md bg-[#D9D9D9] rounded-xl  lg:mt-0 lg:mr-16 xl:mr-24 lg:mb-20 xl:mb-20" />
+          <div className="h-[30vh] justify-self-center self-start lg:self-center  w-11/12 sm:w-8/12 lg:w-4/12 text-[12px] lg:text-[14.38px] text-justify font-medium font-poppins leading-[36.75px] text-white lg:ml-16 xl:ml-16 lg:mb-28 xl:mb-32">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
