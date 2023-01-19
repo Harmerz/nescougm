09158de8d7c1a2147c3bda3199be228e43b5d8ad
Anantadhirya <anantadhirya@gmail.com>
2023-01-19 10:14:24 +0700

    fix : changed electricity to video competition

--- pages/competition/[cabang].js
+++ pages/competition/[cabang].js
@@ -38,29 +38,6 @@ export function Competition({ props }) {
   )
 }
 
-const Electricity = {
-  competitionTitle: 'Electricity Competition',
-  competitionDescription:
-    'Paper Competition merupakan kompetisi bidang ketenagalistrikan berupa lomba problem solving tentang Load Flow Diagram, forecasting, sistem proteksi listrik, dan sejenisnya, yang bertujuan mendorong perkembangan dan perencanaan sistem ketenagalistrikan, khususnya di Indonesia.',
-  peserta: 'Mahasiswa, perseorangan atau tim maksimal 3 orang',
-  biayaPendaftaran: 'Rp 150.000,00-',
-  contactPersons: [
-    ['Fajar', '081390803682', 'fajars7710'],
-    ['Satria', '083195784338', 'satriayw'],
-  ],
-  juaraMahasiswa: ['Rp5.000.000,-', 'Rp4.000.000,-', 'Rp3.000.000,-'],
-  juaraSMA: ['Rp5.000.000,-', 'Rp4.000.000,-', 'Rp3.000.000,-', 'Rp250.000,-'],
-  timelines: [
-    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
-    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
-    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
-    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
-    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
-    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
-    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
-  ],
-}
-
 const Paper = {
   competitionTitle: 'Paper Competition',
   competitionDescription:
@@ -107,10 +84,33 @@ const Poster = {
   ],
 }
 
+const Video = {
+  competitionTitle: 'Video Competition',
+  competitionDescription:
+    'Paper Competition merupakan kompetisi bidang ketenagalistrikan berupa lomba problem solving tentang Load Flow Diagram, forecasting, sistem proteksi listrik, dan sejenisnya, yang bertujuan mendorong perkembangan dan perencanaan sistem ketenagalistrikan, khususnya di Indonesia.',
+  peserta: 'Mahasiswa, perseorangan atau tim maksimal 3 orang',
+  biayaPendaftaran: 'Rp 150.000,00-',
+  contactPersons: [
+    ['Fajar', '081390803682', 'fajars7710'],
+    ['Satria', '083195784338', 'satriayw'],
+  ],
+  juaraMahasiswa: ['Rp5.000.000,-', 'Rp4.000.000,-', 'Rp3.000.000,-'],
+  juaraSMA: ['Rp5.000.000,-', 'Rp4.000.000,-', 'Rp3.000.000,-', 'Rp250.000,-'],
+  timelines: [
+    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
+    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
+    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
+    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
+    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
+    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
+    ['xx Mei - xx Juni', 'Mahasiswa, perseorangan atau tim maksimal 3 orang'],
+  ],
+}
+
 export default function CompetitionPage() {
   const router = useRouter()
   const Cabang = router.query.cabang
-  if (Cabang === 'electricity') return <Competition props={Electricity} />
+  if (Cabang === 'video') return <Competition props={Video} />
   if (Cabang === 'paper') return <Competition props={Paper} />
   if (Cabang === 'poster') return <Competition props={Poster} />
   return (
