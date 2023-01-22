1fb94e54e584f313eb9d4f206e89c445f49632ce
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-01-23 03:24:50 +0700

    fix: add comingsoon page to webinar

--- pages/webinar.js
+++ pages/webinar.js
@@ -1,11 +1,12 @@
 import { BackToTop } from '../components/element/BackToTop'
 import { DefaultLayout } from '../components/layout'
+import { Comingsoon } from '../components/pages/comingsoon'
 
 export default function Webinar() {
   return (
     <DefaultLayout title="Webinar - NESCO UGM 2023">
-      <div className="justify-center bg-gradient-to-br from-bg-00 to-bg-01 h-[100vh] flex items-center">
-        <div className="text-7xl font-bold uppercase text-white">Webinar</div>
+      <div>
+        <Comingsoon />
         <BackToTop />
       </div>
     </DefaultLayout>
