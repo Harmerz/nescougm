1a7e0af9dc2e67a9b279a8595053a00c4b814214
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-01-29 02:21:52 +0700

    fix: update index admin dashboard

--- pages/dashboard/admin/index.js
+++ pages/dashboard/admin/index.js
@@ -1,12 +1,13 @@
 import { BackToTop } from '../../../components/element/BackToTop'
 import DefaultLayoutDashboard from '../../../components/layoutDashboard/DefaultLayoutDashboard'
-import { Hero } from '../../../components/pages/dashboard/admin/Hero'
+import { CompetitionList, Hero } from '../../../components/pages/dashboard/admin'
 
 export default function DashboardAdmin() {
   return (
     <DefaultLayoutDashboard title="Dashboard - NESCO UGM 2023">
-      <div>
+      <div className="bg-c-00 min-h-[100vh] pt-[150px] pb-[100vh] lg:py-[150px] px-[5%] lg:px-[100px] lg:overflow-hidden">
         <Hero />
+        <CompetitionList />
         <BackToTop />
       </div>
     </DefaultLayoutDashboard>
