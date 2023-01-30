a155acc007247b0d8b613b57a4de3525e6153044
Anantadhirya <78126769+Anantadhirya@users.noreply.github.com>
2023-01-30 20:33:09 +0700

    fix : dashboard navbar link

--- components/layoutDashboard/NavbarDashboard.js
+++ components/layoutDashboard/NavbarDashboard.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-undef */
 import Image from 'next/image'
+import Link from 'next/link'
 
 import logo from '../../public/NescoDashboard.svg'
 import { Button } from '../element/button'
@@ -8,17 +8,17 @@ export function NavbarDashboard() {
   return (
     <div className="w-[100vw] z-[9999] bg-c-00">
       <div className="flex justify-between items-center px-[20px] md:px-[100px] py-[48px]">
-        <a href="./">
+        <Link href="/">
           <Button type="secondary" size="md" className="">
             Return to Homepage
           </Button>
-        </a>
-        <a
-          href="./"
+        </Link>
+        <Link
+          href="/"
           className="hidden md:flex transition duration-300 ease-in-out hover:scale-125 transform active:scale-105 active:opacity-50 "
         >
           <Image src={logo} alt="logo" />
-        </a>
+        </Link>
       </div>
     </div>
   )
