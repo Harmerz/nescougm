9eed622d94ecdca5b11dd0f949bcc3679a276aa8
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-02-04 02:55:32 +0700

    feat : popup content

--- components/pages/dashboard/admin/CompetitionList.js
+++ components/pages/dashboard/admin/CompetitionList.js
@@ -1,9 +1,53 @@
 import Image from 'next/image'
+import { useCallback, useState } from 'react'
+
+import { Button } from '../../../element/button'
 
 export function CompetitionList() {
+  const [warning, setWarning] = useState(false)
+  const [competition, setCompetition] = useState('')
+
+  const ChangeContent = useCallback(() => {
+    return (
+      <div className="flex justify-center">
+        <div className="flex flex-col gap-[15px] justify-center bg-[#22292F] border-c-02/25 border-[1px] px-[78px] py-[48px] rounded-[10px] text-white">
+          <h1 className=" text-center text-[14px] leading-[30px] font-poppins font-medium">
+            Apakah Anda sudah yakin untuk mendaftar pada cabang lomba {competition} ini?
+            <p className="text-[9px] font-poppins font-light">
+              Peringatan: satu akun hanya dapat didaftarkan pada satu cabang lomba
+            </p>
+          </h1>
+          <div className="flex justify-center gap-[40px]">
+            <div>
+              <Button onClick={() => setWarning(!warning)} size="sm" type="secondary">
+                Batalkan
+              </Button>
+            </div>
+            <button
+              type="submit"
+              onClick={() => {
+                setWarning(!warning)
+              }}
+            >
+              <Button size="sm" type="primary">
+                Daftar
+              </Button>
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }, [competition, warning])
   return (
-    <div className="flex md:flex-row mt-12 justify-around flex-col">
-      <div className="svg-wrapper relative flex justify-center">
+    <div className="flex md:flex-row mt-12 justify-around flex-col relative">
+      <button
+        type="button"
+        onClick={() => {
+          setWarning(!warning)
+          setCompetition('Paper Competition')
+        }}
+        className="svg-wrapper relative flex justify-center"
+      >
         <svg height="224" width="200" strokeLinecap="round">
           <rect
             className="competition-shape fill-bg-04"
@@ -39,9 +83,16 @@ export function CompetitionList() {
             Paper Competition
           </div>
         </div>
-      </div>
+      </button>
 
-      <div className="svg-wrapper relative flex justify-center">
+      <button
+        type="button"
+        onClick={() => {
+          setWarning(!warning)
+          setCompetition('Video Competition')
+        }}
+        className="svg-wrapper relative flex justify-center"
+      >
         <svg height="224" width="200" strokeLinecap="round">
           <rect
             className="competition-shape fill-bg-04"
@@ -77,8 +128,15 @@ export function CompetitionList() {
             Video Competition
           </div>
         </div>
-      </div>
-      <div className="svg-wrapper relative flex justify-center">
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setWarning(!warning)
+          setCompetition('Poster Competition')
+        }}
+        className="svg-wrapper relative flex justify-center"
+      >
         <svg height="224" width="200" strokeLinecap="round">
           <rect
             className="competition-shape fill-bg-04"
@@ -114,6 +172,9 @@ export function CompetitionList() {
             Poster Competition
           </div>
         </div>
+      </button>
+      <div className="absolute z-40 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
+        {warning ? <ChangeContent /> : <div />}
       </div>
     </div>
   )
