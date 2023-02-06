569af09a80cb2165ea4658eece0f8683b90b9225
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-02-06 17:46:32 +0700

    feat : lazy import

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,28 @@
-// import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
 
 import { BackToTop } from '../components/element/BackToTop'
 import { DefaultLayout, Footer } from '../components/layout'
-import {
-  About,
-  Hero,
-  HomePageCompetition,
-  Medpart,
-  Photo,
-  Webinar,
-} from '../components/pages/homepage'
+import { Hero } from '../components/pages/homepage'
 
-// const HomePageCompetition = lazy(() => import('../components/pages/homepage/Competition'))
-// const About = lazy(() => import('../components/pages/homepage/About'))
-// const Medpart = lazy(() => import('../components/pages/homepage/Medpart'))
-// const Photo = lazy(() => import('../components/pages/homepage/Photo'))
-// const Webinar = lazy(() => import('../components/pages/homepage/Webinar'))
+const HomePageCompetition = lazy(() => import('../components/pages/homepage/Competition'))
+const About = lazy(() => import('../components/pages/homepage/About'))
+const Medpart = lazy(() => import('../components/pages/homepage/Medpart'))
+const Photo = lazy(() => import('../components/pages/homepage/Photo'))
+const Webinar = lazy(() => import('../components/pages/homepage/Webinar'))
 
 export default function Home() {
   return (
     <DefaultLayout title="Homepage - NESCO UGM 2023">
       <div>
         <Hero />
-        <About />
-        <HomePageCompetition />
-        <Webinar />
-        <Photo />
-        <Medpart />
-        <BackToTop />
+        <Suspense fallback={<div>Loading...</div>}>
+          <About />
+          <HomePageCompetition />
+          <Webinar />
+          <Photo />
+          <Medpart />
+          <BackToTop />
+        </Suspense>
       </div>
 
       <footer>
