5fe6be5f5758979ac017371181da05ebc70d7977
Annisa Uswa Sufia <annisauswasufia@gmail.com>
2023-02-10 08:16:37 +0700

    fix: auth

--- components/element/button/Button.js
+++ components/element/button/Button.js
@@ -9,7 +9,7 @@ export const Button = ({ children, type, size, animation, onClick }) => {
     sm: 'text-sm px-7 py-2',
     md: 'text-base px-8 py-2',
     lg: 'text-base px-12 py-3 ',
-    xl: 'text-base w-[362px] h-[51px]',
+    xl: 'text-base w-[319px] sm:w-[362px] h-[51px]',
   }
   const buttonAnimation = {
     main: 'drop-shadow-[0_0_14px_rgba(109,199,236,0.491667)] hover:drop-shadow-[0_0_20px_rgba(81,177,204,0.64)]',
--- pages/auth/verif1/index.js
+++ pages/auth/verif1/index.js
@@ -1,10 +1,11 @@
+import { DefaultLayoutAuth } from '../../../components/layoutAuth'
 import { Verifikasi } from '../../../components/pages/auth/verif1'
 
 export function VerifikasiPage() {
   return (
-    <div>
+    <DefaultLayoutAuth title="Sign Up - NESCO UGM 2023">
       <Verifikasi />
-    </div>
+    </DefaultLayoutAuth>
   )
 }
 
