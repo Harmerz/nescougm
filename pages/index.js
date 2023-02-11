fd4bfb8373602212fd1843db51d2a912df28d6ad
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-02-11 19:33:12 +0700

    fix : back to normal

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,19 @@ import { lazy, Suspense } from 'react'
 
 import { BackToTop } from '../components/element/BackToTop'
 import { DefaultLayout, Footer } from '../components/layout'
+import { Hero } from '../components/pages/homepage/Hero'
 
 const HomePageCompetition = lazy(() => import('../components/pages/homepage/Competition'))
 const About = lazy(() => import('../components/pages/homepage/About'))
 const Medpart = lazy(() => import('../components/pages/homepage/Medpart'))
+const Photo = lazy(() => import('../components/pages/homepage/Photo'))
 const Webinar = lazy(() => import('../components/pages/homepage/Webinar'))
 
 export default function Home() {
   return (
     <DefaultLayout title="Homepage - NESCO UGM 2023">
       <div>
+        <Hero />
         <Suspense fallback={<div>Loading About</div>}>
           <About />
           <BackToTop />
@@ -21,6 +24,9 @@ export default function Home() {
         </Suspense>
         <Suspense fallback={<div>Loading Webinar</div>}>
           <Webinar />
+        </Suspense>
+        <Suspense fallback={<div>Loading Photo</div>}>
+          <Photo />
         </Suspense>
         <Suspense fallback={<div>Loading Medpart</div>}>
           <Medpart />
